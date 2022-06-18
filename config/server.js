4ce678f67b3bf682acc6ff1a49140701f5551de5
David Lucas <david.lucas@umvel.com>
2022-06-18 13:26:21 -0500

    chore: deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7209d832dec06aecd1f60d4d5186408'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e7c338d9-1813-42e7-a465-3d52d54bbf27'
+};
